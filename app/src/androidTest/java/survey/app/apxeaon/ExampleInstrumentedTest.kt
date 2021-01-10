eb9cb916c2de5844fad418d88c66fb407966743d
Paulormb <56367122+Paulormb@users.noreply.github.com>
2021-01-10 03:59:46 +0000

    Update "Fragment main"

--- app/src/androidTest/java/survey/app/apxeaon/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/survey/app/apxeaon/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package survey.app.apxeaon
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("nam.tran.apxeaon", appContext.packageName)
+        assertEquals("survey.app.apxeaon", appContext.packageName)
     }
 }
